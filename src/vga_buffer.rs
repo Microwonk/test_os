d168c2f8c167ea190897537a71a02adb9119f2c7
Nicolas Frey <nicolas.theo.frey@gmail.com>
2023-06-21 23:46:16 +0200

    self made unit tests

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -26,7 +26,7 @@ pub fn _print(args: fmt::Arguments) {
 lazy_static! {
     pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
         column_position: 0,
-            color_code: ColorCode::new(Color::Yellow, Color::Black),
+            color_code: ColorCode::new(Color::Green, Color::Black),
             buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
     });
 }
@@ -145,5 +145,29 @@ impl fmt::Write for Writer { // for macros
     fn write_str(&mut self, s: &str) -> fmt::Result {
         self.write_string(s);
         Ok(())
+    }
+}
+
+
+// tests
+#[test_case]
+fn test_println_simple() {
+    println!("test_println_simple output");
+}
+
+#[test_case]
+fn test_println_many() {
+    for _ in 0..200 {
+        println!("test_println_many output");
+    }
+}
+
+#[test_case]
+fn test_println_output() {
+    let s = "Some test string that fits on a single line";
+    println!("{}", s);
+    for (i, c) in s.chars().enumerate() {
+        let screen_char = WRITER.lock().buffer.chars[BUFFER_HEIGHT - 2][i].read();
+        assert_eq!(char::from(screen_char.ascii_character), c);
     }
 }
